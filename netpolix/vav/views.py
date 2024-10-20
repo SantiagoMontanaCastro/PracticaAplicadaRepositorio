377e8897baba3086ace9fe65be524ec9874d7fe4
santiago sanchez <ivsasafo30@hotmail.com>
2024-10-20 16:56:19 -0500

    carrito de compras

--- netpolix/vav/views.py
+++ netpolix/vav/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
-from django.shortcuts import get_object_or_404, redirect
-from .models import Video
-from .carrito import Carrito  # Importamos la clase Carrito
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Video, Carrito 
 from .models import AlquilerVenta
+from django.contrib import messages  
+
 # Create your views here.
 
 
@@ -23,3 +23,25 @@ def alquilar_venta(request, video_id):
     # Por ejemplo, puedes crear un nuevo objeto AlquilerVenta si es necesario
 
     return render(request, 'vav/alquilar_venta.html', {'video': video})
+
+
+def agregar_al_carrito(request, video_id, tipo):
+    video = get_object_or_404(Video, id=video_id)
+
+    # Verificar si el video ya está en el carrito del usuario con el mismo tipo (alquiler/venta)
+    carrito_item = Carrito.objects.filter(usuario=request.user, video=video, tipo=tipo).first()
+
+    if carrito_item:
+        # Si el video ya está en el carrito, mostrar un mensaje de advertencia
+        messages.warning(request, f"El video '{video.titulo_original}' ya está en tu carrito como {tipo}.")
+    else:
+        # Si no está, lo agregamos al carrito
+        Carrito.objects.create(usuario=request.user, video=video, tipo=tipo)
+        messages.success(request, f"El video '{video.titulo_original}' ha sido agregado a tu carrito como {tipo}.")
+
+    return redirect('ver_carrito')  # Redirigir a la página de carrito o donde desees
+
+def ver_carrito(request):
+    carrito_items = Carrito.objects.filter(usuario=request.user)
+    total = sum(item.video.precio_alquiler if item.tipo == 'alquiler' else item.video.precio_venta for item in carrito_items)
+    return render(request, 'vav/carrito.html', {'carrito_items': carrito_items, 'total': total})
