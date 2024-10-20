8133862846b1512916073c3514e2dc60b7d46c44
Cristian <cristian@gmail.com>
2024-10-20 16:25:26 -0500

    Implementacion Login

--- netpolix/crm/urls.py
+++ netpolix/crm/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import registro_cliente
-
+from .views import registro_cliente, ingreso_usuario
 urlpatterns = [
-    path('registro/', registro_cliente, name='registro_cliente'),  # URL para el registro
+    path('registro/', registro_cliente, name='registro_cliente'),
+    path('login/',ingreso_usuario),   # URL para el registro
     # otras rutas...
 ]
--- netpolix/crm/views.py
+++ netpolix/crm/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from .forms import RegistroClienteForm
+from django.contrib.auth.forms import AuthenticationForm
 # Create your views here.
 
 
@@ -20,3 +21,25 @@ def registro_cliente(request):
         form = RegistroClienteForm()
     return render(request, 'crm/registro.html', {'form': form})
 
+
+def ingreso_usuario(request):
+
+    if request.method == 'GET':
+        return render(request, 'crm/login.html', {
+            'form': AuthenticationForm()
+        })
+
+    else:
+        # Autenticar al usuario con el username y password proporcionado
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        
+        if user is not None:
+            # Si la autenticación es correcta, iniciar sesión y redirigir
+            login(request, user)
+            return redirect('/vav/catalogo')
+        else:
+            # Si la autenticación falla, mostrar un error
+            return render(request, 'crm/login.html', {
+                'form': AuthenticationForm(),
+                'error': 'Invalid username or password'
+            })
